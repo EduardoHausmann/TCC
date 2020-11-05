e435663169f25d0279e23058c415d7910a00f98b
EduardoHausmann <eduardohausmann1705@gmail.com>
2020-11-05 13:34:53 -0300

    Feita a codificaçãod a Tela de Funcionário

--- TCC-Musica/View/frmFuncionario.Designer.cs
+++ TCC-Musica/View/frmFuncionario.Designer.cs
@@ -28,20 +28,274 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label lblUsuario;
+            System.Windows.Forms.Label lblSenha;
+            System.Windows.Forms.Label lblCargo;
+            System.Windows.Forms.Label lblSalario;
+            System.Windows.Forms.Label lblContato;
+            this.funcionarioBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dgvFuncionario = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.mbSalario = new System.Windows.Forms.MaskedTextBox();
+            this.mbContato = new System.Windows.Forms.MaskedTextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            lblUsuario = new System.Windows.Forms.Label();
+            lblSenha = new System.Windows.Forms.Label();
+            lblCargo = new System.Windows.Forms.Label();
+            lblSalario = new System.Windows.Forms.Label();
+            lblContato = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.funcionarioBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionario)).BeginInit();
             this.SuspendLayout();
+            // 
+            // funcionarioBindingSource
+            // 
+            this.funcionarioBindingSource.DataSource = typeof(Musica.DAL.Funcionario);
+            // 
+            // dgvFuncionario
+            // 
+            this.dgvFuncionario.AllowUserToAddRows = false;
+            this.dgvFuncionario.AllowUserToDeleteRows = false;
+            this.dgvFuncionario.AutoGenerateColumns = false;
+            this.dgvFuncionario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFuncionario.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6});
+            this.dgvFuncionario.DataSource = this.funcionarioBindingSource;
+            this.dgvFuncionario.Location = new System.Drawing.Point(16, 173);
+            this.dgvFuncionario.Name = "dgvFuncionario";
+            this.dgvFuncionario.ReadOnly = true;
+            this.dgvFuncionario.RowHeadersVisible = false;
+            this.dgvFuncionario.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFuncionario.Size = new System.Drawing.Size(504, 220);
+            this.dgvFuncionario.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Código";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Usuario";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Usuário";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Cargo";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Cargo";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Salario";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Salário";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "Contato";
+            this.dataGridViewTextBoxColumn6.HeaderText = "Contato";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblUsuario.Location = new System.Drawing.Point(12, 9);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new System.Drawing.Size(64, 20);
+            lblUsuario.TabIndex = 1;
+            lblUsuario.Text = "Usuário";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Usuario", true));
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(16, 32);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(242, 26);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // lblSenha
+            // 
+            lblSenha.AutoSize = true;
+            lblSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblSenha.Location = new System.Drawing.Point(281, 9);
+            lblSenha.Name = "lblSenha";
+            lblSenha.Size = new System.Drawing.Size(56, 20);
+            lblSenha.TabIndex = 3;
+            lblSenha.Text = "Senha";
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Senha", true));
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(285, 32);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.PasswordChar = '*';
+            this.txtSenha.Size = new System.Drawing.Size(235, 26);
+            this.txtSenha.TabIndex = 4;
+            // 
+            // lblCargo
+            // 
+            lblCargo.AutoSize = true;
+            lblCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblCargo.Location = new System.Drawing.Point(12, 61);
+            lblCargo.Name = "lblCargo";
+            lblCargo.Size = new System.Drawing.Size(52, 20);
+            lblCargo.TabIndex = 5;
+            lblCargo.Text = "Cargo";
+            // 
+            // txtCargo
+            // 
+            this.txtCargo.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Cargo", true));
+            this.txtCargo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCargo.Location = new System.Drawing.Point(16, 84);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(170, 26);
+            this.txtCargo.TabIndex = 6;
+            // 
+            // lblSalario
+            // 
+            lblSalario.AutoSize = true;
+            lblSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblSalario.Location = new System.Drawing.Point(210, 61);
+            lblSalario.Name = "lblSalario";
+            lblSalario.Size = new System.Drawing.Size(58, 20);
+            lblSalario.TabIndex = 7;
+            lblSalario.Text = "Salário";
+            // 
+            // mbSalario
+            // 
+            this.mbSalario.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Salario", true));
+            this.mbSalario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mbSalario.Location = new System.Drawing.Point(214, 84);
+            this.mbSalario.Mask = "9,999.99";
+            this.mbSalario.Name = "mbSalario";
+            this.mbSalario.Size = new System.Drawing.Size(118, 26);
+            this.mbSalario.TabIndex = 8;
+            // 
+            // lblContato
+            // 
+            lblContato.AutoSize = true;
+            lblContato.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblContato.Location = new System.Drawing.Point(363, 61);
+            lblContato.Name = "lblContato";
+            lblContato.Size = new System.Drawing.Size(66, 20);
+            lblContato.TabIndex = 9;
+            lblContato.Text = "Contato";
+            // 
+            // mbContato
+            // 
+            this.mbContato.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.funcionarioBindingSource, "Contato", true));
+            this.mbContato.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mbContato.Location = new System.Drawing.Point(367, 84);
+            this.mbContato.Mask = "(99)99999-9999";
+            this.mbContato.Name = "mbContato";
+            this.mbContato.Size = new System.Drawing.Size(153, 26);
+            this.mbContato.TabIndex = 10;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(223, 133);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(98, 34);
+            this.btnCancelar.TabIndex = 13;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(324, 133);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 34);
+            this.btnExcluir.TabIndex = 12;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(144, 133);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 34);
+            this.btnSalvar.TabIndex = 11;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
             // 
             // frmFuncionario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(532, 401);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(lblContato);
+            this.Controls.Add(this.mbContato);
+            this.Controls.Add(lblSalario);
+            this.Controls.Add(this.mbSalario);
+            this.Controls.Add(lblCargo);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(lblSenha);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(lblUsuario);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.dgvFuncionario);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmFuncionario";
-            this.Text = ".: Funcionario :.";
+            this.Text = ".: Funcionário :.";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmFuncionario_FormClosed);
+            this.Load += new System.EventHandler(this.frmFuncionario_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.funcionarioBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFuncionario)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource funcionarioBindingSource;
+        private System.Windows.Forms.DataGridView dgvFuncionario;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.MaskedTextBox mbSalario;
+        private System.Windows.Forms.MaskedTextBox mbContato;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnSalvar;
     }
 }
--- TCC-Musica/View/frmFuncionario.cs
+++ TCC-Musica/View/frmFuncionario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Musica.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,73 @@ namespace View
         public frmFuncionario()
         {
             InitializeComponent();
+        }
+
+        private void frmFuncionario_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            this.funcionarioBindingSource.CancelEdit();
+            MeusFormularios.FormFuncionario = null;
+        }
+
+        private void frmFuncionario_Load(object sender, EventArgs e)
+        {
+            this.funcionarioBindingSource.DataSource = DataContextFactory.DataContext.Funcionario;
+            this.funcionarioBindingSource.AddNew();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (this.Valida())
+            {
+                this.funcionarioBindingSource.EndEdit();
+                DataContextFactory.DataContext.SubmitChanges();
+                dgvFuncionario.Refresh();
+                MessageBox.Show("Armazenado com Sucesso!");
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.funcionarioBindingSource.CancelEdit();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Tem certeza?", "Confirmação!", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.funcionarioBindingSource.RemoveCurrent();
+                DataContextFactory.DataContext.SubmitChanges();
+                dgvFuncionario.Refresh();
+                MessageBox.Show("Excluido com Sucesso!");
+            }
+        }
+
+        private bool Valida()
+        {
+            if (txtUsuario.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("O campo usúario é obrigatório");
+                return false;
+            }
+            else if (txtSenha.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("O campo senha é obrigatório");
+                return false;
+            }
+            else if (txtCargo.Text.Trim() == string.Empty)
+            {
+                MessageBox.Show("O campo cargo é obrigatório");
+                return false;
+            }
+            return true;
+        }
+
+        public Funcionario FuncionarioCorrente
+        {
+            get
+            {
+                return (Funcionario)this.funcionarioBindingSource.Current;
+            }
         }
     }
 }
