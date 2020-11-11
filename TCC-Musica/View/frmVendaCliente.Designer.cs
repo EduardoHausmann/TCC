8ecf41cf6dec07cefdde87ae4481cd5fa52f6f3b
EduardoHausmann <eduardohausmann1705@gmail.com>
2020-11-11 09:36:27 -0300

    Feita a codificação da Tela de Vendas por Cliente

--- TCC-Musica/View/frmVendaCliente.Designer.cs
+++ TCC-Musica/View/frmVendaCliente.Designer.cs
@@ -28,20 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label lblCliente;
+            this.vendaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dgvVenda = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cbCliente = new System.Windows.Forms.ComboBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.clienteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            lblCliente = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.vendaBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVenda)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).BeginInit();
             this.SuspendLayout();
+            // 
+            // vendaBindingSource
+            // 
+            this.vendaBindingSource.DataSource = typeof(Musica.DAL.Venda);
+            // 
+            // dgvVenda
+            // 
+            this.dgvVenda.AllowUserToAddRows = false;
+            this.dgvVenda.AllowUserToDeleteRows = false;
+            this.dgvVenda.AutoGenerateColumns = false;
+            this.dgvVenda.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVenda.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dgvVenda.DataSource = this.vendaBindingSource;
+            this.dgvVenda.Location = new System.Drawing.Point(12, 66);
+            this.dgvVenda.Name = "dgvVenda";
+            this.dgvVenda.ReadOnly = true;
+            this.dgvVenda.RowHeadersVisible = false;
+            this.dgvVenda.Size = new System.Drawing.Size(406, 285);
+            this.dgvVenda.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "Id";
+            this.Column1.HeaderText = "Código";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "Valor";
+            this.dataGridViewTextBoxColumn2.HeaderText = "Valor";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Desconto";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Desconto";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "ValorPago";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Valor Pago";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.ReadOnly = true;
+            // 
+            // lblCliente
+            // 
+            lblCliente.AutoSize = true;
+            lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblCliente.Location = new System.Drawing.Point(12, 9);
+            lblCliente.Name = "lblCliente";
+            lblCliente.Size = new System.Drawing.Size(58, 20);
+            lblCliente.TabIndex = 1;
+            lblCliente.Text = "Cliente";
+            // 
+            // cbCliente
+            // 
+            this.cbCliente.DataBindings.Add(new System.Windows.Forms.Binding("SelectedValue", this.vendaBindingSource, "IdCliente", true));
+            this.cbCliente.DataSource = this.clienteBindingSource;
+            this.cbCliente.DisplayMember = "Nome";
+            this.cbCliente.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCliente.FormattingEnabled = true;
+            this.cbCliente.Location = new System.Drawing.Point(12, 32);
+            this.cbCliente.Name = "cbCliente";
+            this.cbCliente.Size = new System.Drawing.Size(293, 28);
+            this.cbCliente.TabIndex = 2;
+            this.cbCliente.ValueMember = "Id";
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPesquisar.Location = new System.Drawing.Point(311, 29);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(88, 31);
+            this.btnPesquisar.TabIndex = 3;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // clienteBindingSource
+            // 
+            this.clienteBindingSource.DataSource = typeof(Musica.DAL.Cliente);
             // 
             // frmVendaCliente
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(425, 357);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(lblCliente);
+            this.Controls.Add(this.cbCliente);
+            this.Controls.Add(this.dgvVenda);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "frmVendaCliente";
             this.Text = ".: Venda por Cliente :.";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmVendaCliente_FormClosed);
+            this.Load += new System.EventHandler(this.frmVendaCliente_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.vendaBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVenda)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clienteBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource vendaBindingSource;
+        private System.Windows.Forms.DataGridView dgvVenda;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.ComboBox cbCliente;
+        private System.Windows.Forms.BindingSource clienteBindingSource;
+        private System.Windows.Forms.Button btnPesquisar;
     }
 }
--- TCC-Musica/View/frmVendaCliente.cs
+++ TCC-Musica/View/frmVendaCliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Musica.DAL;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,27 @@ namespace View
         public frmVendaCliente()
         {
             InitializeComponent();
+        }
+
+        private void frmVendaCliente_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MeusFormularios.FormVendaCliente = null;
+        }
+
+        private void frmVendaCliente_Load(object sender, EventArgs e)
+        {
+            this.vendaBindingSource.DataSource = DataContextFactory.DataContext.Venda;
+            this.clienteBindingSource.DataSource = DataContextFactory.DataContext.Cliente;
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            this.Pesquisar((int)cbCliente.SelectedValue);
+        }
+
+        public void Pesquisar(int idCliente)
+        {
+            this.vendaBindingSource.DataSource = DataContextFactory.DataContext.Venda.Where(x => x.IdCliente == idCliente);
         }
     }
 }
